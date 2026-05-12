095f8430f42c54ee525d8b8729644222da892cd9
ECEB0704 <gogli.chavez@gmail.com>
2026-05-12 05:12:42 -0500

    feat(routes): add IoT infrastructure routes for device management and calibration

---
+++ src/iot-infrastructure/presentation/iot-infrastructure-routes.js
@@ -0,0 +1,9 @@
+﻿const home = () => import('./views/iot-management.view.vue');
+const calibration = () => import('./views/iot-calibration.view.vue');
+
+const iotInfrastructureRoutes = [
+    { path: '', name: 'iot-infrastructure-home', component: home, meta: { title: 'Gestión de Dispositivos IoT' } },
+    { path: 'calibrate/:id', name: 'iot-calibration', component: calibration, meta: { title: 'Calibración de Dispositivo' } }
+];
+
+export default iotInfrastructureRoutes;
