8b139ffa78a7924f7c952ed7574a3ce5b650d80c
Johan Contreras <j.contreras.granados.04@gmail.com>
2026-05-12 05:08:56 -0500

    feat: add ordering module with OrderList component for order management

---
+++ src/order-management/presentation/views/ordering-moule.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ordering-container">
+
+    <OrderList />
+  </div>
+</template>
+
+<script setup>
+import OrderList from "../components/order-list.vue";
+</script>
+
+<style scoped>
+.ordering-container {
+  height: 100%;
+}
+</style>
