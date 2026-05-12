0dbf22912587534169c0cb0870354ecfbb224c13
ECEB0704 <gogli.chavez@gmail.com>
2026-05-12 05:21:22 -0500

    feat: add Vehicle entity definition for Profiles & Vehicles context

---
+++ src/profiles-and-vehicles/domain/model/vehicle.entity.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Vehicle entity within the Profiles & Vehicles bounded context.
+ *
+ * @class Vehicle
+ */
+export class Vehicle {
+    constructor({ id = null, plate = '', type = '', capacityKg = 0, hasRefrigeration = false, assignedDriverId = null, lastMaintenanceDate = null } = {}) {
+        this.id = id;
+        this.plate = plate;
+        this.type = type;
+        this.capacityKg = capacityKg;
+        this.hasRefrigeration = hasRefrigeration;
+        this.assignedDriverId = assignedDriverId;
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
+}
