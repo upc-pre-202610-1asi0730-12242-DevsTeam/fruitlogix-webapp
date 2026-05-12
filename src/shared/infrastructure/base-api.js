f4d5ed1402dc2e57474b8a9056df8bc6fa601e47
Johan Contreras <j.contreras.granados.04@gmail.com>
2026-05-12 05:18:38 -0500

    feat: add BaseApi class with pre-configured Axios instance

---
+++ src/shared/infrastructure/base-api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+
+/**
+ * Base infrastructure class that creates and exposes a pre-configured Axios instance.
+ * All bounded-context API gateways extend this class.
+ *
+ * @class BaseApi
+ */
+export class BaseApi {
+    /** @type {import('axios').AxiosInstance} */
+    #http;
+
+    /**
+     * Initialises the Axios instance with the platform base URL and default headers.
+     */
+    constructor() {
+        this.#http = axios.create({
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+    }
+
+    /**
+     * The shared Axios HTTP client.
+     * @returns {import('axios').AxiosInstance}
+     */
+    get http() {
+        return this.#http;
+    }
+}
